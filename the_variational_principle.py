4f2318786480a0fa7440ed7d39fffa366887efb1
Tiernan8r <tiernan8r@pm.me>
2020-02-21 15:00:21 +0000

    * Real Constants * Handling of Infinite Square Well case. * Plotting of results.

--- the_variational_principle.py
+++ the_variational_principle.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 import numpy
 
 # global constants:
-hbar = 1
-m = 1
+hbar = 6.582119569 * 10 ** -16  # 6.582119569×10−16 (from wikipedia)
+m = 9.1093837015 * 10 ** -31  # 9.1093837015(28)×10−31
 factor = -(hbar ** 2) / (2 * m)
 
 
 def normalise(psi: numpy.ndarray, dx: float):
     # integrate using the rectangular rule
-    norm = numpy.sum(psi * psi) * dx
+    norm = numpy.nansum(psi * psi) * dx
     norm_psi = psi / numpy.sqrt(norm)
     return norm_psi
 
@@ -33,13 +33,15 @@ def second_derivative(f: numpy.ndarray, i: int, dx: float):
 
 def energy(psi: numpy.ndarray, V: numpy.ndarray, dx: float):
     total_energy = 0
-    # for i in range(1, len(psi) - 1):
-    for i in range(len(psi)):
-        T = factor * second_derivative(psi, i, dx)
-        Vp = V[i] * psi[i]
-        E = psi[i] * (T + Vp)
+    for i in range(1, len(psi) - 1):
 
-        total_energy += E
+        if not numpy.isnan(V[i]) or numpy.isinf(V[i]):
+            T = factor * second_derivative(psi, i, dx)
+            # T = factor * (psi[i - 1] - 2 * psi[i] + psi[i + 1]) * (dx ** -2)
+            Vp = V[i] * psi[i]
+            E = psi[i] * (T + Vp)
+
+            total_energy += E
 
     return total_energy * dx
 
@@ -47,17 +49,27 @@ def energy(psi: numpy.ndarray, V: numpy.ndarray, dx: float):
 # def energy(psi: numpy.ndarray, V: numpy.ndarray, dx: float):
 #     Vp = V * psi
 #     Tp = factor * numpy.diff(psi, 2, append=0, prepend=0)
-#     return numpy.sum(psi * (Tp + Vp)) * dx
+#     return numpy.nansum(psi * (Tp + Vp)) * dx
 
 
 def potential(x: numpy.ndarray):
-    return 0.5 * x ** 2
+    # return 0.5 * x ** 2
 
+    length = len(x)
+    third = length // 3
+    # mid, bef = numpy.zeros(third + 1), numpy.linspace(numpy.inf, numpy.inf, third)
+    mid, bef = numpy.zeros(third + 1) + 0.3 * numpy.linspace(0, (x[1] - x[0]) * (third + 1), third + 1), numpy.linspace(
+        10, 10, third)
+    aft = bef.copy()
+    return numpy.concatenate((bef, mid, aft))
+
+    # return 0.5 * x**2 + x
 
 def main():
     random.seed("THE-VARIATIONAL-PRINCIPLE")
 
     a, b, N = -10, 10, 100
+    # a, b, N = 0, 10, 100
     dx = (b - a) / N
     x = numpy.linspace(a, b, N)
 
@@ -65,20 +77,20 @@ def main():
     psi[0], psi[-1] = 0, 0
     V = potential(x)
 
-    scale = 100
-    plt.plot(x, psi)
+    scale = 10
+    # plt.plot(x, psi)
     psi = normalise(psi, dx)
 
     plt.plot(x, V)
-    plt.plot(x, psi * scale)
+    # plt.plot(x, psi * scale)
 
     prev_E = energy(psi, V, dx)
     print("Initial Energy:", prev_E)
 
     num_iterations = 100000
     for i in range(num_iterations):
-        # rand_x = random.randrange(1, N - 1)
-        rand_x = random.randrange(N)
+        rand_x = random.randrange(1, N - 1)
+        # rand_x = random.randrange(N)
         rand_y = random.random() * 0.1 * (num_iterations - i) / num_iterations
 
         if random.random() > 0.5:
@@ -95,12 +107,33 @@ def main():
             psi = normalise(psi, dx)
 
     plt.plot(x, psi * scale)
-    plt.title("$\psi$ functions")
+    # plt.plot(x, psi * 10 ** 5)
+
+    plt.title("Ground State $\psi$ for the Perturbed Finite Square Well:")
     plt.xlabel("x")
     plt.ylabel("$\psi$")
-    plt.legend(("Original $\psi$", "potential", "Normalised $\psi$", "Final $\psi$"))
+    # plt.legend(("Original $\psi$", "potential", "Normalised $\psi$", "Final $\psi$"))
+    plt.legend(("Potential", "Ground State"))
+    # plt.legend(("Ground State", "Analytical Solution"))
     plt.show()
     print("Final Energy:", prev_E)
+
+    # #Comparison for actual inf sq well:
+    # third = N // 3
+    # psi_rel = psi[third:2 * third + 1]
+    #
+    # L = len(psi_rel) * dx
+    # x_rel = numpy.linspace(0, L, len(psi_rel))
+    # psi_anal = normalise(numpy.sin(numpy.pi / L * x_rel), dx)
+    #
+    # print(len(psi_rel))
+    # plt.plot(x_rel, psi_rel)
+    # plt.plot(x_rel, psi_anal)
+    # plt.title("Comparison of Analytical to Computed $\psi$ for the Infinite Square Well:")
+    # plt.xlabel("x")
+    # plt.ylabel("$\psi$")
+    # plt.legend(("Computed", "Analytical"))
+    # plt.show()
 
 
 main()
