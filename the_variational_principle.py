6e2ce6a735d61d572b8d905b84e6d1049deb457f
Tiernan8r <Tiernan8r@pm.me>
2020-05-14 16:11:25 +0100

    Update & Reorganisation of base project: * changed requirements.txt to remove specific versioning. * Renamed lagrangian -> laplacian, my bad  - LAG -> DEV2 for simplicity. * Reorganised the main() method to call a compute() method as an intermediary that handles all the calculations, variables are defined in main() * Changing some plottings() behaviour to include legends and comparison between psi & V.

--- the_variational_principle.py
+++ the_variational_principle.py
@@ -8,9 +8,9 @@ import scipy.linalg as la
 import scipy.sparse as spr
 
 # global constants:
-hbar = 6.582119569 * 10 ** -16  # 6.582119569x10^-16 (from wikipedia)
+hbar = 6.5821189 * 10 ** -16  # 6.582119569x10^-16 eV (from wikipedia)
 # electron
-m = 9.1093826 * 10 ** -31  # 9.1093837015(28)x10^-31
+m = 9.1093819 * 10 ** -31  # 9.1093837015(28)x10^-31
 # factor used in calculation of energy
 factor = -(hbar ** 2) / (2 * m)
 
@@ -18,11 +18,11 @@ factor = -(hbar ** 2) / (2 * m)
 th = {1: "st", 2: "nd", 3: "rd"}
 
 # The Lagrangian Derivative matrix
-global LAG
+global DEV2
 # a list of names for the 1st 10 axes.
 axes = ("x", "y", "z", "w", "q", "r", "s", "t", "u", "v")
 # The name of the QM system to be shown in the plot titles.
-pot_sys_name = "Infinite Square Well"
+pot_sys_name = "Harmonic Oscillator"
 # the colour map for the surface plots.
 colour_map = "autumn"
 
@@ -67,28 +67,28 @@ def dev_mat(D: int, N: int, axis_number: int, dr: float) -> np.ndarray:
     return D_n * (dr ** -2)
 
 
-def generate_lagrangian(D: int, N: int, dr: float):
+def generate_laplacian(D: int, N: int, dr: float):
     """
     Generates the Lagrangian second derivative matrix for the number of axes D.
     :param D: The number of dimensions/axes in the system.
     :param N: The size of each dimension.
     :param dr: The grid spacing in the system.
     """
-    global LAG
+    global DEV2
 
-    # Initially set LAG to be undefined.
-    LAG = None
+    # Initially set DEV2 to be undefined.
+    DEV2 = None
 
     # iterate over each dimension in the system.
     for ax in range(D):
         # generate the second order central difference matrix for this axis
         D_n = dev_mat(D, N, ax, dr)
-        # if it's the first matrix generated, set LAG equal to it.
-        if LAG is None:
-            LAG = D_n
+        # if it's the first matrix generated, set DEV2 equal to it.
+        if DEV2 is None:
+            DEV2 = D_n
         # otherwise add it, as matrix multiplication is distributive (ie differentiation is distributive)
         else:
-            LAG += D_n
+            DEV2 += D_n
 
 
 def energy(psi: np.ndarray, V: np.ndarray, dr: float) -> float:
@@ -105,8 +105,8 @@ def energy(psi: np.ndarray, V: np.ndarray, dr: float) -> float:
     Vp = np.where(np.isfinite(Vp), Vp, 0)
 
     # Calculate the kinetic energy of the system
-    # LAG is the lagrangian 2nd derivative matrix.
-    Tp = factor * (LAG @ psi)
+    # DEV2 is the lagrangian 2nd derivative matrix.
+    Tp = factor * (DEV2 @ psi)
 
     # Return the integral of the KE and PE applied to psi, which is the energy.
     return (psi * (Tp + Vp)).sum() * dr
@@ -118,21 +118,152 @@ def potential(r: np.ndarray) -> np.ndarray:
     :param r: The coordinate grid of the system for each axis.
     :return: The potential function V as a grid of values for each position.
     """
-    V = r.copy()
-    for i in range(len(r)):
-        length = len(r[i])
-        third = length // 3
 
-        addition = int(abs((third - (length / 3))) * 3)
+    N = r.shape[1]
+    D = r.shape[0]
 
-        mid, bef = np.zeros(third + addition), np.linspace(np.inf, np.inf, third)
-        # mid, bef = np.zeros(third + addition), np.linspace(10, 10, third)
-        aft = bef.copy()
-        # return np.concatenate((bef, mid, aft))
-        V[i] = np.concatenate((bef, mid, aft))
+    # # Infinite Sq Well
+    # #V_0 = np.inf
+    # # Finite Sq Well
+    # V_0 = 10
+    #
+    # third = N // 3
+    #
+    # addition = int(abs((third - (N / 3))) * 3)
+    #
+    # mid, bef = np.zeros(third + addition), np.linspace(V_0, V_0, third)
+    # aft = bef.copy()
+    # x_well = np.concatenate((bef, mid, aft))
+    # wells = [x_well] * D
+    # V = np.array(np.meshgrid(*wells, indexing="ij"))
+    #
+    # # Correction of Corners:
+    # V = np.sum(V, axis=0)
+    # V = V.reshape(N ** D)
+    # # Perturbation
+    # R = np.sum(r, axis=0)
+    # R = R.reshape(N ** D)
+    # for i in range(len(V)):
+    #     if V[i] > 0:
+    #         V[i] = V_0
+    #     # Perturbation
+    #     elif V[i] == 0:
+    #         V[i] += 0.5 * R[i]
+    # V = V.reshape([N] * D)
 
+    # # Alpha Barrier Penetration
+    # V = r.copy()
+    # for i in range(len(r)):
+    #     length = len(r[i])
+    #     third = length // 3
+    #
+    #     Z = 48
+    #     e = 1.6021753*10**-19
+    #     e_0 = 8.854187817*10**-12
+    #     pi = np.pi
+    #
+    #     # # Rc = 1.2 A^1.3 fm
+    #     # Rc = r[i][third]
+    #     # A = (Rc / 1.2) ** (1/1.3) * 10**-15
+    #
+    #     # U = -np.array([2.4 * (Z - 2) * A **-(1/3)] * third)
+    #     U = -np.array([0.05] * third)
+    #     U[0] = np.inf
+    #
+    #     # inv_r = ((Z - 2) * 2 * e**2) / (4 * pi * e_0 * r[i][third:])
+    #     inv_r = 1 / r[i][third:]
+    #
+    #     V[i] = np.concatenate((U, inv_r))
+    # V = np.sum(V, axis=0)
+
+    # Harmonic Oscillator:
+    V = 0.5 * r ** 2
+    V = np.sum(V, axis=0)
+
+    # # Anharmonic Oscillator:
+    # V = r + 0.5 * r ** 2 + 0.25 * r **4
+    # V = np.sum(V, axis=0)
+
+    # # V-shaped
+    # V = abs(r)
+    # V = np.sum(V, axis=0)
+
+    # # Inverse R:
+    # V = r ** -1
+    # V = np.sum(V, axis=0)
+
+    # # Inverse Sq Law
+    # V = r ** -2
+    # V = np.sum(V, axis=0)
+
+    # # Free Particle
     # V = np.zeros(r.shape)
-    # V = np.sum(0.5 * r ** 2, axis=0)
+    # V = np.sum(V, axis=0)
+
+    # # # Central Potential:
+    # A = -10
+    # B = 1.5
+    # C = 8
+    #
+    # V_c = A * C * r**-1
+    # V_f = B * C**2 * r**-2
+    #
+    # V = V_c + V_f
+    # V = np.sum(V, axis=0)
+
+    # # Crystal Band Structure:
+    # num_bands = 5
+    # band_spacing = N // (2 * num_bands)
+    # V_0 = 10
+    #
+    # x_band = np.linspace(V_0, V_0, N)
+    # # for i in range(1, 2 * num_bands, 2):
+    # i = 0
+    # j = -1
+    # for k in range(N):
+    #     i %= band_spacing
+    #     j %= 2
+    #     if j == 0:
+    #         x_band[k] = 0
+    #     if i == 0:
+    #         j += 1
+    #     i += 1
+    #
+    # # for i in range(num_bands + 2):
+    # #     start_index = band_spacing * i * 2
+    # #     for j in range(band_spacing):
+    # #         x_band[start_index + j] = 0
+    #
+    # wells = [x_band] * D
+    # V = np.array(np.meshgrid(*wells, indexing="ij"))
+    #
+    # # Correction of Corners:
+    # V = np.sum(V, axis=0)
+    # V = V.reshape(N ** D)
+    # # # Perturbation
+    # # R = np.sum(r, axis=0)
+    # # R = R.reshape(N ** D)
+    # for i in range(len(V)):
+    #     if V[i] > 0:
+    #         V[i] = V_0
+    #     # # Perturbation
+    #     # elif V[i] == 0:
+    #     #     V[i] += 0.5 * R[i]
+    # V = V.reshape([N] * D)
+
+    # # Delta Barrier:
+    # V = np.zeros(r.shape)
+    # sub_V = V
+    # while True:
+    #     sub_sub_V = sub_V[0]
+    #     if not type(sub_sub_V) is np.ndarray:
+    #         L = len(sub_V)
+    #         sub_V[L // 2] = -np.inf
+    #         break
+    #     else:
+    #         sub_V = sub_sub_V
+    # V = V.sum(axis=0)
+
     return V
 
 
@@ -164,7 +295,8 @@ def nth_state(r: np.ndarray, dr: float, D: int, N: int, num_iterations: int,
     V = V.reshape(N ** D)
 
     # generate an initial psi, I've found that a quadratic function works nicely (no discontinuities.)
-    psi = 0.5 * r ** 2
+    psi = (0.5 * r ** 2).sum(axis=0)
+    # psi = np.ones(r.shape).sum(axis=0)
 
     # linearise psi from a grid to a column vector
     psi = psi.reshape(N ** D)
@@ -174,15 +306,18 @@ def nth_state(r: np.ndarray, dr: float, D: int, N: int, num_iterations: int,
     # Keep track of all the indices that have an inf value for the V.
     nan_indices = [False] * len_V
     for j in range(len_V):
+        # # Tag the bordering points as well.
+        # a, b = j - 1, j + 1
+        # if a < 0:
+        #     a = 0
+        # if b >= len_V:
+        #     b = len_V - 1
+        #
+        # if not np.isfinite(V[j]) and (not np.isfinite(V[a]) and not np.isfinite(V[b])):
+        #     # nan_indices[a] = nan_indices[j] = nan_indices[b] = True
+        #     nan_indices[j] = True
         if not np.isfinite(V[j]):
-            # Tag the bordering points as well.
-            a, b = j - 1, j + 1
-            if a < 0:
-                a = 0
-            if b >= len_V:
-                b = len_V - 1
-
-            nan_indices[a] = nan_indices[j] = nan_indices[b] = True
+            nan_indices[j] = True
 
     # filter the corresponding psi values to be = 0
     psi = np.where(nan_indices, 0, psi)
@@ -247,7 +382,7 @@ def nth_state(r: np.ndarray, dr: float, D: int, N: int, num_iterations: int,
     return psi
 
 
-def plotting(r, all_psi, D, include_V=False, V=None):
+def plotting(r, all_psi, D, include_V=False, V=None, V_scale=1):
     """
     A method that contains various forms of plotting of psi and the potential of the system.
     :param r: The grid coordinates.
@@ -260,10 +395,10 @@ def plotting(r, all_psi, D, include_V=False, V=None):
     cmap = plt.cm.get_cmap(colour_map)
 
     # A method to plot the 1D system as a line.
-    def plot_line(x, y, title, ylabel="$\psi$", legend=None, filename=None, include_V=False, V=None):
+    def plot_line(x, y, title, ylabel="$\psi$", legend=None, filename=None, include_V=False, V=None, V_scale=1):
         if include_V:
             plt.plot(x, V)
-        plt.plot(x, y)
+        plt.plot(x, y * V_scale)
         plt.xlabel("$x$")
         plt.ylabel(ylabel)
         plt.title(title)
@@ -331,26 +466,30 @@ def plotting(r, all_psi, D, include_V=False, V=None):
             state_names += ["{}{} State".format(i, th.get(i, "th"))]
 
         # If we want to plot the potential, plot it first.
-        if include_V:
-            all_psi = np.vstack((V, all_psi))
-            state_names = ["Potential"] + state_names
+        all_psi = np.vstack(([V], all_psi))
+        state_names = ["Potential"] + state_names
 
         # iterate over all the functions to plot, and plot them individually.
         for i in range(len(all_psi)):
             title = "The {} for the {} along $x$:".format(state_names[i], pot_sys_name)
             file_name = "state_{}".format(i - 1)
             state = "$\psi$"
+
             plt_with_V = include_V
+            with_V_scale = V_scale
             if state_names[i] == "Potential":
                 file_name = "potential"
                 state = "V"
                 plt_with_V = False
+                with_V_scale = 1
 
             legend = [state_names[i]]
-            if include_V:
+            if plt_with_V:
                 legend = [state_names[0]] + legend
             legend = tuple(legend)
-            plot_line(*r, all_psi[i], title, state, legend=legend, include_V=plt_with_V, V=V[0], filename=file_name)
+
+            plot_line(*r, all_psi[i], title, state, legend=legend, include_V=plt_with_V, V=V, V_scale=with_V_scale,
+                      filename=file_name)
 
     # If the system is 2D, plot the img, wireframe and surfaces.
     elif D == 2:
@@ -373,34 +512,40 @@ def plotting(r, all_psi, D, include_V=False, V=None):
 
         if include_V:
             title = "The Potential function for the {} along $x$, $y$ & $z$".format(pot_sys_name)
+            N = V.shape[1]
+            D = len(V.shape)
+            V = V.reshape(N ** D)
             plot_3D_scatter(*r, V, title)
 
         num_states = len(all_psi)
         for n in range(num_states):
             title = "$\psi_{}$ for the {} along $x$, $y$ & $z$".format(n, pot_sys_name)
-            plot_3D_scatter(*r, all_psi[n], title)
+
+            N = all_psi[n].shape[1]
+            D = len(all_psi[n].shape)
+            psi = all_psi[n].reshape(N ** D)
+
+            plot_3D_scatter(*r, psi, title)
 
     # All higher order systems can't be easily visualised.
     else:
         return
 
 
-def main():
-    # The size and range of the grid
-    start, stop, N = -10, 10, 100
-    # The number of orders of psi to calculate
-    num_states = 1
+def compute(start=-10, stop=10, N=100, D=1, num_states=1, num_iterations=10 ** 7):
+    """
+    The method to set up the variables and system, and aggregate the computed wavefunctions.
+    :param start: The lower bound of the grid.
+    :param stop: The upper bound of the grid.
+    :param N: The number of samples along an axis.
+    :param D: The number of dimensions.
+    :param num_states: The number of wavefunctions to compute.
+    :param num_iterations: The number of iterations per computation.
+    :return: r, V, all_psi: the grid, potential function and the list of all the wavefunctions.
+    """
     # Keep the number of states in bounds, so that the orthonormal basis generator doesn't return an error.
     if num_states >= N:
         num_states = N - 2
-
-    # The number of axes of the system
-    D = 1
-    # Iteration recommendations:
-    # 10**5 for 1D
-    # 10**6 for 2D
-    # 10**7 for 3D (loooong)
-    num_iterations = 10 ** 5
 
     # The coordinates along the x axis
     x = np.linspace(start, stop, N)
@@ -416,15 +561,15 @@ def main():
     dr = (stop - start) / N
 
     # Generate the 2nd order finite difference derivative matrix.
-    generate_lagrangian(D, N, dr)
+    generate_laplacian(D, N, dr)
 
     # Keep track whether we are on the first iteration or not.
     first_iteration = True
     # Set up two arrays to store the generated psi:
     # Stores the psi as linear column vectors, used for calculating the next psi in the series.
-    all_psi_linear = np.zeros([1] + [N ** D])
+    all_psi_linear = np.zeros((1, N ** D))
     # stores in their proper shape as grids, used for plotting.
-    all_psi = np.zeros([1] + [N] * D)
+    all_psi = np.zeros((1, N * D))
 
     # iterate over the number of states we want to generate psi for.
     for i in range(num_states):
@@ -441,10 +586,28 @@ def main():
             all_psi_linear = np.vstack((all_psi_linear, [psi_linear]))
             all_psi = np.vstack((all_psi, [psi]))
 
+    return r, V, all_psi
+
+
+def main():
     # Whether to plot the potential function or not.
     include_potential = True
+
+    # The size and range of the grid
+    start, stop, N = -10, 10, 100
+    # The number of orders of psi to calculate
+    num_states = 1
+    # The number of axes of the system
+    D = 1
+    # Number of times to generate samples in the wavefunction
+    num_iterations = 10 ** 5
+
+    # a factor to scale the psi by when plotting it together with the potential function in the 1D case.
+    v_scale = 10
+
+    r, V, all_psi = compute(start, stop, N, D, num_states, num_iterations)
     # plot the generated psis.
-    plotting(r, all_psi, D, include_potential, V)
+    plotting(r, all_psi, D, include_potential, V, v_scale)
 
 
 main()
