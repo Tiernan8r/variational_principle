62558f4b1c937f5c4e45991165f459d82beb77a7
Tiernan8r <tiernan8r@pm.me>
2021-08-20 09:39:25 +0100

    outstanding tweaks (#46)

---
+++ variational_principle/constants.py
@@ -0,0 +1,7 @@
+
+# global constants:
+HBAR = 6.5821189 * 10 ** -16  # 6.582119569x10^-16 eV (from wikipedia)
+# electron
+MASS = 9.1093819 * 10 ** -31  # 9.1093837015(28)x10^-31
+# factor used in calculation of energy
+ENERGY_FACTOR = -(HBAR ** 2) / (2 * MASS)
